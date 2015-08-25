e827f7d5be361ce7e8b4d57420b567ce834f1939
Santiago Basulto <santiago.basulto@gmail.com>
2015-08-25 09:55:08 -0300

    Non interactive mode by default

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def show_results(results):
         click.echo("\n:'( No results")
         return
 
-    click.echo("\n##### Successful Hunt! are your emails #####")
+    click.echo("\n##### Successful Hunt! Here are your emails #####")
     click.echo("------------------------------------\n")
     for result in results:
         click.echo("Email: '{}'".format(result.email))
@@ -22,12 +22,16 @@ def show_results(results):
 @click.option('--handle', '-h', required=True,
               help="Twitter handle (eg: @rmotr_com).")
 @click.option('--domain', '-d', required=False,
-              help="Domain of emails. Will look for emails just in that domain")
-@click.option('--consumer-key', '-k', envvar='TWITTER_CONSUMER_KEY', required=True,
+              help="Will look for emails just with that domain")
+@click.option('--consumer-key', '-k',
+              envvar='TWITTER_CONSUMER_KEY', required=True,
               help="Create an app and get your keys from dev.twitter.com")
-@click.option('--secret-key', '-s', envvar='TWITTER_SECRET_KEY', required=True,
+@click.option('--secret-key', '-s',
+              envvar='TWITTER_SECRET_KEY', required=True,
               help="Create an app and get your keys from dev.twitter.com")
-def interactive_search_emails(handle, domain=None, consumer_key=None, secret_key=None):
+@click.option('--interactive/--no-interactive', default=False)
+def interactive_search_emails(handle, domain, consumer_key,
+                              secret_key, interactive):
     auth = tweepy.AppAuthHandler(consumer_key, secret_key)
     api = tweepy.API(auth)
     results = []
@@ -37,6 +41,9 @@ def interactive_search_emails(handle, domain=None, consumer_key=None, secret_key
         for result in search_emails(api, handle, domain):
             if result not in results:
                 results.append(result)
+                if not interactive:
+                    click.echo("Found {}".format(result.email))
+                    continue
                 if click.confirm(msg.format(result.email)):
                     raise KeyboardInterrupt()
     except KeyboardInterrupt:
